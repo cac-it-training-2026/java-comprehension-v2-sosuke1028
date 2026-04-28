ec5c8df4bf3783b1e67f17ebf409265306af0894
marutaso <marutaso@C010423582.zipangu.cac.co.jp>
2026-04-28 12:10:15 +0900

    quession1完了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question01/Member.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question01/Member.java
@@ -9,53 +9,11 @@ class Member {
 	int age;
 	int rank;
 
-	/**
-	 * @return name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name セットする name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @return age
-	 */
-	public int getAge() {
-		return age;
-	}
-
-	/**
-	 * @param age セットする age
-	 */
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	/**
-	 * @return rank
-	 */
-	public int getRank() {
-		return rank;
-	}
-
-	/**
-	 * @param rank セットする rank
-	 */
-	public void setRank(int rank) {
-		this.rank = rank;
-	}
-
 	public void showMember() {
 		System.out.println("***MEMBER DATA***");
-		System.out.println("name:" + getName());
-		System.out.println("age:" + getAge());
-		System.out.println("rank:" + getRank());
+		System.out.println("name:" + name);
+		System.out.println("age:" + age);
+		System.out.println("rank:" + rank);
 		System.out.println("*****************");
 
 	}
--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question01/SystemMain01.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question01/SystemMain01.java
@@ -12,16 +12,29 @@ rank:1
 *****************
 ***MEMBER DATA***
 name:Sato Kensuke
-age:36
+
+*****************age:36
 rank:2
-*****************
 
  * 
  */
 public class SystemMain01 {
 
 	public static void main(String[] args) {
+
+		System.out.println("---SHOW DATA---");
 		//TODO ここから実装する
+		Member member1 = new Member();
+		member1.name = "Miura Manabu";
+		member1.age = 24;
+		member1.rank = 1;
+		member1.showMember();
+
+		Member member2 = new Member();
+		member2.name = "Sato Kensuke";
+		member2.age = 36;
+		member2.rank = 2;
+		member2.showMember();
 
 	}
 
