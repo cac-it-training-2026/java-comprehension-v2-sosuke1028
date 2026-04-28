a90db8d7aabcdec4c2077400e720fee5ba304795
marutaso <marutaso@C010423582.zipangu.cac.co.jp>
2026-04-28 13:49:02 +0900

    quesiion3完了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
@@ -3,7 +3,11 @@ package q01_basic.question03;
 public class SystemMain03 {
 
 	public static void main(String[] args) {
-		//TODO ここから実装する
+		Member member1 = new Member();
+		Member member2 = new Member("Miura Manabu", 28, 2);
+
+		member1.showMember();
+		member2.showMember();
 
 	}
 
